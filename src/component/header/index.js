d50f7a8b6c1be56e2cdbc25db7afe9a5dbcc178b
Wangzihao5325 <416990273@qq.com>
2019-07-02 15:03:15 +0700

    1.优化

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+
 export default class Header extends Component {
     render() {
         return (
-            <div style={{ position: 'fixed', left: 0, top: 0, width: '100%', height: 38, backgroundColor: 'rgb(3,3,3)' }}></div>
+            <div style={{ position: 'fixed', left: 0, top: 0, width: '100%', height: 38, backgroundColor: 'rgb(3,3,3)', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <div onClick={this.goBack} style={{ marginLeft: 12 }}><img src={require('../../image/left.png')} style={{ height: 22, width: 22 }} alt='' /></div>
+            </div>
         );
+    }
+
+    goBack = () => {
+        console.log('goback');
     }
 }
