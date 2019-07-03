505cad4a99c0c45ec6d2baea726b36b5b2edeab8
Wangzihao5325 <416990273@qq.com>
2019-07-03 19:36:16 +0700

    1.处理短片小说底部tab展示

--- src/component/bottom/index.js
+++ src/component/bottom/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as Params from '../../global/param';
 
 class ItemBtn extends Component {
 
@@ -35,13 +36,13 @@ function mapState2Props(store) {
 
 const ItemBtnWithStore = connect(mapState2Props)(ItemBtn);
 
-export default class Footer extends Component {
+class Footer extends Component {
 
     render() {
         return (
-            <div style={{ display: 'flex', flexDirection: 'row', height: 64, width: '100%', position: 'fixed', bottom: 0, left: 0, backgroundColor: 'rgb(3,3,3)' }}>
-                <ItemBtnWithStore key='menu_list' type='menu_list' icon={require('../../image/chapter_list.png')} callback={this.showChapterList} title='目录' />
-                <ItemBtnWithStore key='progress' type='progress' icon={require('../../image/progress.png')} callback={this.progress} title='进度' />
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 64, width: '100%', position: 'fixed', bottom: 0, left: 0, backgroundColor: 'rgb(3,3,3)' }}>
+                {this.props.fictionType === Params.Nnovel && <ItemBtnWithStore key='menu_list' type='menu_list' icon={require('../../image/chapter_list.png')} callback={this.showChapterList} title='目录' />}
+                {this.props.fictionType === Params.Nnovel && <ItemBtnWithStore key='progress' type='progress' icon={require('../../image/progress.png')} callback={this.progress} title='进度' />}
                 <ItemBtnWithStore key='setting' type='setting' icon={require('../../image/font.png')} callback={this.setting} title='设置' />
                 <ItemBtnWithStore key='mode' type='mode' type='special' icon2={require('../../image/moon.png')} icon={require('../../image/sun.png')} callback={this.primary} title='日间' />
             </div>
@@ -68,3 +69,12 @@ export default class Footer extends Component {
         }
     }
 }
+
+function mapState2PropsTwo(store) {
+    return {
+        fictionType: store.data.fictionType
+    }
+}
+
+const FooterWithStore = connect(mapState2PropsTwo)(Footer);
+export default FooterWithStore;
